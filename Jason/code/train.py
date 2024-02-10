329169c6cfc0e4a0e91eda92e0f97e3c874b5bb0
jeraimondi <113797492+jeraimondi@users.noreply.github.com>
2024-02-10 02:52:42 -0500

    Update requirements and train code files

--- Jason/code/train.py
+++ Jason/code/train.py
@@ -1,6 +1,8 @@
 import argparse
 import os
+import shutil
 import torch
+from ultralytics import settings
 from ultralytics import YOLO
 
 if __name__ =='__main__':
@@ -10,8 +12,9 @@ if __name__ =='__main__':
     # hyperparameters sent by the client are passed as command-line arguments to the script
     parser.add_argument('--data', type=str, default='data.yaml') # yaml config file for custom dataset
     parser.add_argument('--epochs', type=int, default=3) # number of training epochs
+    parser.add_argument('--batch', type=int, default=-1) # batch size, -1 for AutoBatch
     parser.add_argument('--yolo-model', type=str, default='yolov8n.pt') # pretrained base model
-    parser.add_argument('--saved-model-name', type=str, default='model.pt') # name for model export
+    parser.add_argument('--saved-model-weights', type=str, default='model.pt') # saved model weights
     
     # data and model directories
     parser.add_argument('--output-data-dir', type=str, default=os.environ['SM_OUTPUT_DATA_DIR'])
@@ -24,8 +27,26 @@ if __name__ =='__main__':
     model = YOLO(args.yolo_model)
 
     # train model
-    results = model.train(data=args.data, epochs=args.epochs)
+    results = model.train(data=args.data, epochs=args.epochs, batch=args.batch)
     
-    # save model - parameters only in state_dict
-    model_path = os.path.join(args.model_dir, args.saved_model_name)
-    torch.save(model.state_dict(), model_path)
+    # define soure/dest paths for best model weights
+    path_best_model_source = f"{settings['runs_dir']}/detect/train/weights/best.pt"
+    path_best_model_dest = os.path.join(args.model_dir, args.saved_model_weights)
+    
+    # copy best model weights file for packaging
+    shutil.copy(path_best_model_source, path_best_model_dest)
+    
+    # evaluate model on test dataset
+    print('EVALUATING MODEL ON TEST DATASET...')
+    model_val = YOLO(path_best_model_dest)
+    metrics = model_val.val(data=args.data, split='test')
+    
+    # print evaluation metric to compare performance between models
+    print('-------------')
+    print('-------------')
+    print('-------------')
+    print('MODEL EVALUATION METRIC:')
+    print('mAP50:', round(metrics.box.map50, 4))
+    print('-------------')
+    print('-------------')
+    print('-------------')
